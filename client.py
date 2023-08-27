42655ad1110886a7cf45bca070f1e7bb2dd2e3c8
Sachin ST <itssachinst@gmail.com>
2023-08-27 09:20:32 +0530

    client server program

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ import socket
 
 
 def client_program():
-    host = socket.gethostname()  # as both code is running on same pc
+    host = "192.168.0.98"#socket.gethostname()  # as both code is running on same pc
     port = 5000  # socket server port number
 
     client_socket = socket.socket()  # instantiate
